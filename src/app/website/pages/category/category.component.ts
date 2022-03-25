f38d6c6b4113142e145d2967b83b5ddbc3c2d249
Gian Carlos <giansolis93@gmail.com>
2022-03-24 20:49:28 -0500

    creando layout

--- src/app/website/pages/category/category.component.ts
+++ src/app/website/pages/category/category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 import { switchMap } from 'rxjs';
-import { ProductsService } from '../../services/products.service';
+import { ProductsService } from '../../../services/products.service';
 
 @Component({
   selector: 'app-category',
